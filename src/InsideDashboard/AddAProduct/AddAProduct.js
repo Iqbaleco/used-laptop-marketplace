94ff73f8ee17526f82f9caf2b77b357865af2638
Iqbaleco <iqbaleco70@gmail.com>
2022-11-28 23:33:05 +0600

    added some data

---
+++ src/InsideDashboard/AddAProduct/AddAProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddAProduct = () => {
+    return (
+        <div>
+            <h2>Add Product Here</h2>
+        </div>
+    );
+};
+
+export default AddAProduct;
---
+++ src/InsideDashboard/MyProducts/MyProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProducts = () => {
+    return (
+        <div>
+            <h2>All Sellers' Product</h2>
+        </div>
+    );
+};
+
+export default MyProducts;
