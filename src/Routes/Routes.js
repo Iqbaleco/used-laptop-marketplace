427a1f94d8e29eb0eb65718657b73aea52762bb5
Iqbaleco <iqbaleco70@gmail.com>
2022-11-28 23:46:35 +0600

    added some data

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import AddAProduct from "../InsideDashboard/AddAProduct/AddAProduct";
+import AllBuyers from "../InsideDashboard/AllBuyers/AllBuyers";
+import AllSellers from "../InsideDashboard/AllSellers/AllSellers";
 import MyOrders from "../InsideDashboard/MyOrders/MyOrders";
+import MyProducts from "../InsideDashboard/MyProducts/MyProducts";
 import Dashboard from "../Layout/Dashboard/Dashboard";
 import Main from "../Layout/Main/Main";
 import Blog from "../Pages/Blog/Blog";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import AdminRoute from "./AdminRoute/AdminRoute";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import SellerRoute from "./SellerRoute/SellerRoute";
 
 const router = createBrowserRouter([
     {
@@ -36,8 +43,24 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/dashboard',
-                element: <MyOrders></MyOrders>
-            }
+                element: <PrivateRoute><MyOrders></MyOrders></PrivateRoute>
+            },
+            {
+                path: '/dashboard/allbuyers',
+                element: <AdminRoute><AllBuyers></AllBuyers></AdminRoute>
+            },
+            {
+                path: '/dashboard/allsellers',
+                element: <AdminRoute><AllSellers></AllSellers></AdminRoute>
+            },
+            {
+                path: '/dashboard/addaproduct',
+                element: <SellerRoute><AddAProduct></AddAProduct></SellerRoute>
+            },
+            {
+                path: '/dashboard/myproducts',
+                element: <SellerRoute><MyProducts></MyProducts></SellerRoute>
+            },
         ]
     }
 
